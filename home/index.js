dd46b0da1685b19f39cfdb60c76ed97fdb8c03d8
Chandrakant Verma <chandra.verma196@gmail.com>
2023-12-30 14:35:18 +0530

    created final version commit

--- home/index.js
+++ home/index.js
@@ -1,15 +1,22 @@
 console.log('This is my new javascript project')
 
 const API_KEY = 'f6f6f0d1b0569b0f6681bab1cf92fa0a';
-let API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid={API_KEY}'
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather?q='
+const searchBox =  document.querySelector(".search input")
+const searchBtn =  document.querySelector(".search button")
 
-async function geWeatherDetails(){
+async function geWeatherDetails(city){
 
-    let response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=pune'+`&appid=${API_KEY}`);
+    let response = await fetch(API_URL+city+`&appid=${API_KEY}`);
     let responseJson = await response.json();
+    console.log(responseJson);
     document.querySelector(".city").innerHTML = responseJson.name;
-    document.querySelector(".temp").innerHTML = responseJson.main.temp;
+    document.querySelector(".temp").innerHTML = (responseJson.main.temp-273).toFixed(2)+ "°C";
+    document.querySelector(".humidity").innerHTML = responseJson.main.humidity+" %";
+    document.querySelector(".wind").innerHTML = responseJson.wind.speed + " Km/h";
     //document.querySelector(".temp").innerHTML = responseJson.main.temp;
 
 }
-geWeatherDetails();
+searchBtn.addEventListener("click",()=>{
+    geWeatherDetails(searchBox.value);
+})
